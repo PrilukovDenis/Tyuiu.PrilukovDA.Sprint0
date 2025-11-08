8a60fe579a92985c9cdafec94c8ce1755b77dd4e
deusvela <d.prilukov@aerokod.ru>
2025-11-08 18:06:52 +0500

    Task1

---
+++ Tyuiu.PrilukovD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿
+            Console.WriteLine("Hello, World!");
+
+            string name = "Денис";
+            int age = 23;
+
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+
+            Console.ReadKey();
