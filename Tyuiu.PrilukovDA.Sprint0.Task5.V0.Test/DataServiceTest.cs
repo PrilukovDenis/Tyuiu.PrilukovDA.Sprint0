272980a4559eb444b08edbb864e3bc3f03fe04e2
deusvela <d.prilukov@aerokod.ru>
2025-11-09 20:55:01 +0500

    Task5

---
+++ Tyuiu.PrilukovDA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PrilukovDA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(10, DataService.Subtraction(10, 5));
+        }
+
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(10, DataService.Multiplication(10, 5));
+        }
+
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(10, DataService.Division(9, 3));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PrilukovDA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PrilukovDA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f1c971c-1c3f-4d61-81a3-ea326855f352")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
